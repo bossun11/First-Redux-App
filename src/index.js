cc12db039fcf47e490cccd54543b11af6846ade7
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-03 16:03:55 +0900

    Clean: Reducerを別フォルダに移動

--- src/index.js
+++ src/index.js
@@ -2,41 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
-
-// actions -> increment, decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-// reducers -> アクションと前の状態を受け取って新しい状態に更新する
-const counterReducer = (state = 0, { type }) => {
-  switch (type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-};
-
-// store
-const store = createStore(counterReducer);
-
-//コンソールに新しい状態を表示する
-store.subscribe(() => console.log(store.getState()));
-
-// dispatch
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
---
+++ src/reducers/isLoggin.js
@@ -0,0 +1,10 @@
+const isLogginReducer = (state = false, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return !state;
+    default:
+      return state;
+  }
+};
+
+export default isLogginReducer;
