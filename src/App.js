26f4a0c8ad6082716b612ba82a13b1b606b583f8
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-03 17:07:31 +0900

    Add: 状態更新のためのUIを作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from "./logo.svg";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { increment, decrement, login } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  const handleLogin = () => {
+    dispatch(login());
+  };
+
   return (
     <div className="App">
       <h1>Hello Redux</h1>
+      <h3>カウント：{counter}</h3>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <h3>{isLogin ? "ログインに成功" : "ログインしてください"}</h3>
+      <button onClick={handleLogin}>ログインorログアウト</button>
     </div>
   );
 }
